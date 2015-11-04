30ae2450b8ca8cc22de5d7a6e41f3d41e71beced
philippsxue <phil.xsp@gmail.com>
2015-11-04 13:38:22 -0500

    working version with comments

--- exercise1.py
+++ exercise1.py
@@ -15,16 +15,19 @@ __license__ = "MIT License"
 def pig_latinify(word):
 
     """
-    Describe your function
+    The function is a tool to translate the any English word of input in to Pig Latin word by applying for loop
 
-    :param :
-    :return:
+    :param : The user will enter any English word regardless to its meanings. eg1 (apple) eg2 (welcome)
+    :return: The function will translate the word into a Pig Latin
+            If the first letter is vowel, the letter will be append "yay" eg1 (appleyay)
+            If the first letter is not a vowel, all consonant up to first vowel will be placed to the end, add "ay"
+            eg2 (elcomeway)
     :raises:
 
     """
 
     # Identify a local variable for vowels.
-    vowels = ["a", "e", "i", "o", "u"]
+    vowels = ["a","e", "i", "o", "u"]
 
     # Assigns the appropriate suffix depending if it is a vowel or consonant
     vowel_pig = "yay"
@@ -34,27 +37,35 @@ def pig_latinify(word):
 
     index = 0
     first_vowel = -1
-
     # Part 1: Find the beginning consonant cluster
-    # Check each letter until it reaches a vowel
+    # Use for loop to check each letter until it reaches a vowel
     for letter in word:
         if letter in vowels:
-            # figure out how to stop counting once it reaches a vowel
+            # print "v"
+            # figure out how to stop counting once it reaches a vowel, and set the first vowel as index
             first_vowel = index
             break
-        index += 1
-
+        else:
+            # print "c"
+            index += 1
+    print index
         # Part 2: Make the new word
+        # when there's no vowel found in the word
     if first_vowel == -1:
-            # no vowel was found
-        pig_word = word + consonant_pig
+        # print "con"
+            # return the word with "ay" at the end of the word
+        pig_word = word[:index] + word[index:] + consonant_pig
+            # when the first letter is found to be a vowel
     elif first_vowel == 0:
+            # return the word with "yay" at the end of the word
         pig_word = word + vowel_pig
+            # for other situation, put the consonants up to the first vowel to the end the word, then add "ay"
     else:
-        pig_word = word[first_vowel:] + word[0:first_vowel] + consonant_pig
+         pig_word = word[first_vowel:] + word[0:first_vowel] + consonant_pig
 
     print pig_word
-
+            # the end of the function
     return pig_word
 
+# last but not the least, call the function
 pig_latinify("word")
